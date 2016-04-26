cda36a6a52bf9e8a66b9f3471c83ce39700f85c8
proggeo <mashtaliryurko@gmail.com>
2016-04-26 22:44:45 +0300

    New app showing plans

---
+++ app/src/main/java/com/example/proggeo/android_app_map/Drawing.java
@@ -0,0 +1,50 @@
+package com.example.proggeo.android_app_map;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Picture;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
+
+/**
+ * Created by Proggeo on 4/26/2016.
+ */
+public class Drawing extends View{
+
+    Bitmap plan;
+    Picture picture;
+    Drawable drawable;
+
+    public Drawing(Context context) {
+        super(context);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        plan = BitmapFactory.decodeResource(getResources(), R.drawable.plan4_400,options);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+//        Rect ourRect = new Rect();
+//        ourRect.set(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
+//
+//        Paint blue = new Paint();
+//        blue.setColor(Color.BLUE);
+//        blue.setStyle(Paint.Style.FILL);
+//        canvas.drawRect(ourRect, blue);
+
+//        canvas.drawPicture(picture);
+
+        canvas.drawBitmap(plan, 0, 0, new Paint());
+    }
+}
---
+++ app/src/main/java/com/example/proggeo/android_app_map/MapActivity.java
@@ -0,0 +1,69 @@
+package com.example.proggeo.android_app_map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void switchFloors(View v){
+        ImageView plan = (ImageView)findViewById(R.id.imageView);
+        int floorId=R.drawable.plan1;
+        switch(v.getId()){
+            case R.id.button1:
+                floorId=R.drawable.plan1;
+                break;
+            case R.id.button2:
+                floorId=R.drawable.plan2;
+                break;
+            case R.id.button3:
+                floorId=R.drawable.plan3;
+                break;
+            case R.id.button4:
+                floorId=R.drawable.plan4;
+                break;
+            case R.id.button5:
+                floorId=R.drawable.plan5;
+                break;
+            case R.id.button6:
+                floorId=R.drawable.plan6;
+                break;
+            case R.id.button7:
+                floorId=R.drawable.plan7;
+                break;
+        }
+        plan.setImageResource(floorId);
+
+    }
+}
